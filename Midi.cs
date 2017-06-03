10aa893a6ad3161c2e062f77b6a73e51c5f64155
kloda <mickeh.se@hotmail.com>
2017-06-03 14:37:53 +0200

    First commit.
    Latest update deals with passing mixdown file path to NoteExtractor lib, so that mixdown file name will be notename + ".wav".

---
+++ Midi.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using GisSharpBlog.NetTopologySuite.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Midi
+{
+	public class NoteBsp
+	{
+		List<Note> notes;
+		NoteBsp leftNode;
+		NoteBsp rightNode;
+		int leftBound;
+		int rightBound;
+		public void createNode(int x1, int x2, List<Note> nodeNotes, Song song)
+		{
+			leftBound = x1;
+			rightBound = x2;
+			notes = nodeNotes;
+			int middle = (x2+x1) / 2;
+			if (notes.Count == 0 || middle - x1 < 4 * song.TimeDiv)
+				return;
+			List<Note> leftNoteList = new List<Note>();
+			List<Note> rightNoteList = new List<Note>();
+			foreach (Note note in nodeNotes)
+			{
+				if (note.start < middle && note.stop > x1)
+					leftNoteList.Add(note);
+				if (note.start < x2 && note.stop > middle)
+					rightNoteList.Add(note);
+			}
+			leftNode = new NoteBsp();
+			leftNode.createNode(x1, middle, leftNoteList, song);
+			rightNode = new NoteBsp();
+			rightNode.createNode(middle, x2, rightNoteList, song);
+		}
+		public List<Note> getNotes(int x1, int x2, int minPitch, int maxPitch)
+		{
+			if (leftNode != null && leftNode.leftBound <= x1 && leftNode.rightBound >= x2)
+				return leftNode.getNotes(x1, x2, minPitch, maxPitch);
+			else if (rightNode != null && rightNode.leftBound <= x1 && rightNode.rightBound >= x2)
+				return rightNode.getNotes(x1, x2, minPitch, maxPitch);
+			else
+			{
+				List<Note> selectedNotes = new List<Note>();
+				foreach (Note note in notes)
+				{
+					if (note.pitch >= minPitch && note.pitch <= maxPitch && note.start < x2 && note.stop > x1)
+						selectedNotes.Add(note);
+				}
+				return selectedNotes;
+			}
+		}
+	}
+	public class Event
+	{
+		public byte Type;
+	}
+	public class ChannelEvent : Event
+	{
+		public byte Channel;
+		public byte Param1;
+		public byte Param2;
+	}
+	public class Note
+	{
+		public int start;
+		public int stop;
+		public int channel;
+		public int pitch;
+		public int velocity;
+	}
+	public class MetaEvent : Event
+	{
+		public byte[] Data;
+	}
+	public class Track
+	{
+		public SortedDictionary<int,List<MetaEvent>> MetaEvents { get; set; }
+		public SortedDictionary<int, List<ChannelEvent>> ChannelEvents { get; set; }
+		public List<Note> Notes { get; set; }
+		public NoteBsp NoteBsp { get; set; }
+		public int Length { get; set; }
+		public string Name { get; set; }
+		public Track()
+		{
+			ChannelEvents = new SortedDictionary<int, List<ChannelEvent>>();
+			MetaEvents = new SortedDictionary<int, List<MetaEvent>>();
+			Notes = new List<Note>();
+		}
+		public void addEvent(int time, MetaEvent e)
+		{
+			if (!MetaEvents.ContainsKey(time))
+				MetaEvents.Add(time, new List<MetaEvent>());
+			MetaEvents[time].Add(e);
+		}
+		public void addEvent(int time, ChannelEvent e)
+		{
+			if (!ChannelEvents.ContainsKey(time))
+				ChannelEvents.Add(time, new List<ChannelEvent>());
+			ChannelEvents[time].Add(e);
+		}
+		public List<Note> getNotes(int x1, int x2, int minPitch, int maxPitch)
+		{
+			if (x2 < 0 || x1 > Length)
+				return new List<Note>();
+			if (x1 < 0)
+				x1 = 0;
+			if (x2 >= Length)
+				x2 = Length - 1;
+			return NoteBsp.getNotes(x1, x2, minPitch, maxPitch);
+		}
+		public List<Note> getNotes(int x1, int x2)
+		{
+			return getNotes(x1, x2, 0, 127);
+		}
+	}
+	public class TempoEvent
+	{
+		public int Time { get; set; }
+		public double Tempo { get; set; }
+		public TempoEvent(int _time, double _tempo)
+		{
+			Time = _time;
+			Tempo = _tempo;
+		}
+		public TempoEvent(int _time, int _tempo)
+		{
+			Time = _time;
+			
+		}
+		public TempoEvent(int _time, byte[] _tempo)
+		{
+			Time = _time;
+			setTempo(_tempo);
+		}
+		public void setTempo(byte[] _tempo)
+		{
+			setTempo((_tempo[0] << 16) | (_tempo[1] << 8) | _tempo[2]);
+		}
+		public void setTempo(int _tempo)
+		{
+			Tempo = (float)(60000000.0 / _tempo);
+		}
+	}
+	public partial class Song
+	{
+		int[] pitchStatus = new int[128];
+		byte runningType;
+		byte runningChannel;
+		int totalBytesRead;
+		int chunkBytesRead;
+		int ChunkBytesRead
+		{
+			get
+			{
+				return chunkBytesRead;
+			}
+			set
+			{
+				int dif = value - chunkBytesRead;
+				chunkBytesRead = value;
+				totalBytesRead += dif;
+			}
+		}
+		
+		List<Track> tracks;
+		public List<Track> Tracks { get { return tracks; } set { tracks = value; } }
+		List<TempoEvent> tempoEvents;
+		public List<TempoEvent> TempoEvents { get { return tempoEvents; } set { tempoEvents = value; } }
+		public const float StartTempo = 120;
+		int formatType;
+		public int FormatType { get { return formatType; } }
+		int timeDiv;
+		public int TimeDiv { get { return timeDiv; } set { timeDiv = value; } }
+
+		int songLengthInTicks;
+		public int SongLengthInTicks { get { return songLengthInTicks; } set { songLengthInTicks = value; } }
+		int minPitch;
+		public int MinPitch { get { return minPitch; } }
+		int maxPitch;
+		public int MaxPitch { get { return maxPitch; } }
+		int numPitches;
+		public int NumPitches { get { return numPitches; } }
+				
+		public Song()
+		{
+		}
+		public bool isMidiFile(string path)
+		{
+			using (BinaryReader file = new BinaryReader(File.Open(path, FileMode.Open)))
+			{
+				return file.ReadInt32() == 0x4D546864;
+			}
+		}
+		public void openFile(string path, ref string audioPath, bool modInsTrack, bool mixdown, double songLengthS)
+		{
+			//if (path == null || path == "")
+				//return;
+			//try
+			//{
+			//    using (BEBinaryReader file = new BEBinaryReader(File.Open(path, FileMode.Open)))
+			//    {
+			//    }
+			//}
+			//catch(Exception e)
+			//{
+			//    MessageBox.Show("Couldn't open song file " + path + "\n" + e.Message);
+			//}
+
+			if (!importSongFile(path, ref audioPath, modInsTrack, mixdown, songLengthS))
+			{
+				openMidiFile(path);
+			}
+		}
+
+		public void openMidiFile(string path)
+		{
+			BEBinaryReader file = new BEBinaryReader(File.Open(path, FileMode.Open));
+			using (file)
+			{
+				//Header
+				int headerId = file.ReadInt32();
+				if (headerId != 0x4D546864)
+					throw (new Exception("Unrecognized format."));
+				int headerSize = file.ReadInt32();
+				formatType = (int)file.ReadInt16();
+				int numTracks = (int)file.ReadInt16();
+				timeDiv = (int)file.ReadInt16();
+				songLengthInTicks = 0;
+				maxPitch = 0;
+				minPitch = 127;
+				tracks = new List<Track>();
+				tempoEvents = new List<TempoEvent>();
+				tempoEvents.Add(new TempoEvent(0, 120.0f));
+				totalBytesRead = 14;
+				//Track chunks
+				for (int i = 0; i < numTracks; i++)
+				{
+					//if (i != 9 && i!=0)
+					//continue;
+					tracks.Add(new Track());
+					int chunkId = file.ReadInt32();
+					totalBytesRead += 4;
+					if (chunkId != 0x4D54726B)
+						throw (new Exception("Wrong chunk id for track " + i + "."));
+					int chunkSize = file.ReadInt32();
+					totalBytesRead += 4;
+					chunkBytesRead = 0;
+					int absoluteTime = 0;
+					while (ChunkBytesRead < chunkSize)
+					{
+						readEvent(Tracks.Last(), ref absoluteTime, file, chunkSize);
+					}
+					//totalBytesRead += chunkBytesRead;
+					if (songLengthInTicks < absoluteTime)
+						songLengthInTicks = absoluteTime;
+					if (Tracks.Last().Length < absoluteTime)
+						Tracks.Last().Length = absoluteTime;
+
+					//if (i == 2)
+					//break;
+				}
+
+				numPitches = maxPitch - minPitch + 1;
+			}
+		}
+		
+		int readVarLengthValue(BEBinaryReader stream)
+		{
+			int value = 0;
+			byte b = 128;
+			while ((b & 128) == 128)
+			{
+				b = stream.ReadByte();
+				ChunkBytesRead++;
+				value <<= 7;
+				value |= (b & 127);
+			}
+			return value;
+		}
+		void splitByte(out byte value1, out byte value2, byte b)
+		{
+			value1 = (byte)((b >> 4) & 15);
+			value2 = (byte)(b & 15);
+		}
+		void readEvent(Track track, ref int absoluteTime, BEBinaryReader stream, int chunkSize)
+		{
+			int deltaTime = readVarLengthValue(stream);
+			absoluteTime += deltaTime;
+					
+			byte b = stream.ReadByte();
+			ChunkBytesRead++;
+			if (b == 0xff) //meta event
+			{
+				MetaEvent e = new MetaEvent();
+				int time = absoluteTime;
+				e.Type = stream.ReadByte();
+				ChunkBytesRead++;
+				int length = readVarLengthValue(stream);
+				if (e.Type == 0x3f && length != 0)
+					throw (new Exception("End-of-track event has data length of " + length + ". Should be 0."));
+				e.Data = stream.ReadBytes(length);
+				ChunkBytesRead += length;
+				if (e.Type == 0x51) //Tempo event
+				{
+					TempoEvent te = new TempoEvent(absoluteTime, e.Data);
+					if (!double.IsInfinity(te.Tempo))
+						tempoEvents.Add(te);
+				}
+				else if (e.Type == 0x03)
+				{
+					track.Name = ASCIIEncoding.ASCII.GetString(e.Data);
+				}
+				else
+					track.addEvent(time, e);
+				
+				if (e.Type == 0x2f && ChunkBytesRead != chunkSize)
+					throw (new Exception("End-of-track event at byte "+ChunkBytesRead+" of "+chunkSize+"."));
+				if (e.Type != 0x2f && ChunkBytesRead >= chunkSize)
+					throw (new Exception("End-of-track event missing at end of track."));
+			}
+			else if (b == 0xf0 || b == 0xf7) //sysex
+			{
+				while (b != 0xf7)
+				{
+					b = stream.ReadByte();
+					ChunkBytesRead++;
+				}
+			}
+			else //Channel event
+			{
+				ChannelEvent e = new ChannelEvent();
+				int time = absoluteTime;
+				if (b > 127)
+				{
+					splitByte(out e.Type, out e.Channel, b);
+					runningType = e.Type;
+					runningChannel = e.Channel;
+					e.Param1 = stream.ReadByte();
+					ChunkBytesRead++;
+				}
+				else //Running status
+				{
+					e.Type = runningType;
+					e.Channel = runningChannel;
+					e.Param1 = b;
+				}
+				if (e.Type != 0xc && e.Type != 0xd)
+				{
+					e.Param2 = stream.ReadByte();
+					ChunkBytesRead++;
+				}
+				
+				if (e.Type == 0x9)  //Note on
+				{
+					if (minPitch > e.Param1)
+						minPitch = e.Param1;
+					if (maxPitch < e.Param1)
+						maxPitch = e.Param1;
+					if (e.Param2 == 0)
+						e.Type = 0x8;
+				}
+				if (e.Type == 0x8)
+				{
+					if (pitchStatus[e.Param1] == -1)
+						return;
+
+					Note note = new Note();
+					note.start = pitchStatus[e.Param1];
+					note.stop = absoluteTime;
+					note.channel = e.Channel;
+					note.pitch = e.Param1;
+					note.velocity = e.Param2;
+					for (int i = 0; i <= track.Notes.Count; i++)
+					{
+						if (i == track.Notes.Count || track.Notes[i].start > note.start)
+						{
+							track.Notes.Insert(i, note);
+							break;
+						}
+					}
+					pitchStatus[e.Param1] = -1;
+				}
+				else if (e.Type == 0x9)
+				{
+					pitchStatus[e.Param1] = absoluteTime;
+				}
+				else
+					track.addEvent(time, e);
+				if (ChunkBytesRead >= chunkSize)
+					throw (new Exception("Error at chunk byte "+ChunkBytesRead+" of "+chunkSize+". Last track event is channel event. Shouls be meta event."));
+
+			}
+		}
+		public void createNoteBsp()
+		{
+			foreach (Track track in Tracks)
+			{
+				track.NoteBsp = new Midi.NoteBsp();
+				track.NoteBsp.createNode(0, SongLengthInTicks, track.Notes, this);
+			}
+		}
+	}
+}
---
+++ ModImport.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Midi
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	struct Marshal_TempoEvent
+	{
+		public int time;
+		public double tempo;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	struct Marshal_Note
+	{
+		public int start;
+		public int stop;
+		public int pitch;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	struct Marshal_Track
+	{
+		public IntPtr name;
+		public IntPtr notes;
+		public int numNotes;
+	}
+
+    enum Marshal_SongType { Mod, Sid };
+
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+	struct Marshal_Song
+	{
+		public IntPtr tempoEvents;
+		public int numTempoEvents;
+        //public int timeDiv;
+        public int songLengthT;
+		public int minPitch;
+		public int maxPitch;
+		public IntPtr tracks;
+		public int numTracks;
+        public Marshal_SongType songType;
+	}
+
+	public partial class Song
+	{
+		//static string modWavFileName = Path.GetDirectoryName(Application.ExecutablePath) + "\\music.wav";
+		//static public string ModWavFileName
+		//{
+		//    get { return modWavFileName; }
+		//    //set { modWavFileName = value; }
+		//}
+		static string mixdownDir = "";
+		[DllImport("NoteExtractor.dll", EntryPoint = "initLib", CallingConvention = CallingConvention.Cdecl)]
+		static extern void _initLib();
+		[DllImport("NoteExtractor.dll", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void exitLib();
+		[DllImport("NoteExtractor.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+		static extern bool loadFile(string path, out Marshal_Song marSong, string mixdownPath, bool modInsTrack, double songLengthS);
+		[DllImport("NoteExtractor.dll", EntryPoint = "getMixdownPath", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+		static extern IntPtr getMixdownPath_intptr();
+		public static string getMixdownPath()
+		{
+			return Marshal.PtrToStringAnsi(getMixdownPath_intptr());
+		}
+		public static void initLib(string _mixdownDir)
+		{
+			mixdownDir = _mixdownDir;
+			if (!Directory.Exists(mixdownDir))
+				Directory.CreateDirectory(mixdownDir);
+			_initLib();
+		}
+		public static void deleteMixdowns()
+		{
+			if (Directory.Exists(mixdownDir))
+			{
+				foreach (string file in Directory.GetFiles(mixdownDir))
+					File.Delete(file);
+			}
+		}
+		public static void deleteMixdownDir()
+		{
+			deleteMixdowns();
+			if (Directory.Exists(mixdownDir))
+				Directory.Delete(mixdownDir);
+		}
+
+		bool importSongFile(string path, ref string audioPath, bool modInsTrack, bool mixdown, double songLengthS)
+		{
+            //bool mixdown = audioPath == null || audioPath == "";
+			Marshal_Song marSong;
+			string mixdownPath = mixdown ? Path.Combine(mixdownDir, Path.GetFileName(path))+".wav" : null;
+			if (!loadFile(path, out marSong, mixdownPath, modInsTrack, songLengthS))
+				return false;
+
+			if (mixdown)
+				audioPath = mixdownPath;
+
+			timeDiv = 24;
+			songLengthInTicks = marSong.songLengthT;
+			maxPitch = marSong.maxPitch;
+			minPitch = marSong.minPitch;
+			numPitches = maxPitch - minPitch + 1;
+			tempoEvents = new List<TempoEvent>(marSong.numTempoEvents);
+			for (int i = 0; i < marSong.numTempoEvents; i++)
+			{
+				Marshal_TempoEvent mte = new Marshal_TempoEvent();
+				mte = (Marshal_TempoEvent)Marshal.PtrToStructure(marSong.tempoEvents, typeof(Marshal_TempoEvent));
+				tempoEvents.Add(new TempoEvent(mte.time, mte.tempo));
+                marSong.tempoEvents += Marshal.SizeOf(typeof(Marshal_TempoEvent));
+			}
+			tracks = new List<Track>(marSong.numTracks);
+			for (int i = 0; i < marSong.numTracks; i++)
+			{
+				Marshal_Track mt = new Marshal_Track();
+				mt = (Marshal_Track)Marshal.PtrToStructure(marSong.tracks, typeof(Marshal_Track));
+				tracks.Add(new Track());
+				tracks[i].Name = Marshal.PtrToStringAnsi(mt.name);
+				tracks[i].Notes = new List<Note>(mt.numNotes);
+				tracks[i].Length = marSong.songLengthT;
+				for (int j = 0; j < mt.numNotes; j++)
+				{
+					Marshal_Note mn = new Marshal_Note();
+					mn = (Marshal_Note)Marshal.PtrToStructure(mt.notes, typeof(Marshal_Note));
+					tracks[i].Notes.Add(new Note());
+					tracks[i].Notes[j].pitch = mn.pitch;
+					tracks[i].Notes[j].start = mn.start;
+					tracks[i].Notes[j].stop = mn.stop;
+					mt.notes += Marshal.SizeOf(typeof(Marshal_Note));
+				}
+                marSong.tracks += Marshal.SizeOf(typeof(Marshal_Track));
+			}
+			return true;
+		}
+	}
+}
